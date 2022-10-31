7cc0c0b4540f14c0143bba0e6c4a97c553aca88e
Kaushal Kumar <mail4kaushal.kr@gmail.com>
2022-10-31 11:31:27 +0530

    User Log APP

---
+++ src/components/CustomSearchBar.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function CustomSearchBar() {
+  return (
+	<View>
+	  <Text>CustomSearchBar</Text>
+	</View>
+  )
+}
