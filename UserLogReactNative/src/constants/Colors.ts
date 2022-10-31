e2daf932f0b05311bdc447f81d5a4c4eff3add72
Kaushal Kumar <mail4kaushal.kr@gmail.com>
2022-10-31 18:30:24 +0530

    added project

---
+++ UserLogReactNative/src/constants/Colors.ts
@@ -0,0 +1,11 @@
+export default {
+  WHITE: '#FFFFFF',
+  BLACK: '#000000',
+  HEADER_BACKGROUND: '#1e2749',
+  BTN_BACKGROUND: '#800000',
+  PRIMARY: {
+    BODY: '#fafaff',
+    NOT_SELECTED: '#f9e9ec',
+    SELECTED: '#e4d9ff',
+  },
+};
